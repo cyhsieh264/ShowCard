4c5b6503c589867fd63f6cb7c28d7b79f102a3bd
cyhsieh264 <cyhsieh264@gmail.com>
2020-12-08 14:11:43 +0800

    test setting obj id

--- public/script/card.js
+++ public/script/card.js
@@ -274,8 +274,12 @@ checkUser().then( async (user) => {
     //     width: 200,
     //     top: 200,
     //     left: 200,
-    //     fill: '#fcba03'
+    //     fill: '#fcba03',
+    //     id: 'rec'
     // })
+
+    // console.log(canvas.toJSON(['rec']))
+
     // canvas.add(rect);
     // const rect2 = new fabric.Rect({
     //     height: 100,
@@ -296,11 +300,11 @@ checkUser().then( async (user) => {
     // canvas.loadFromJSON(step.canvas, canvas.renderAll.bind(canvas));
     // {"version":"4.2.0","objects":[{"type":"textbox","version":"4.2.0","originX":"left","originY":"top","left":150,"top":100,"width":300,"height":45.2,"fill":"#8a91ab","stroke":null,"strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeDashOffset":0,"strokeLineJoin":"miter","strokeMiterLimit":4,"scaleX":1,"scaleY":1,"angle":0,"flipX":false,"flipY":false,"opacity":1,"shadow":null,"visible":true,"backgroundColor":"","fillRule":"nonzero","paintFirst":"fill","globalCompositeOperation":"source-over","skewX":0,"skewY":0,"text":"hello world","fontSize":40,"fontWeight":"normal","fontFamily":"Delicious","fontStyle":"normal","lineHeight":1.16,"underline":false,"overline":false,"linethrough":false,"textAlign":"center","textBackgroundColor":"","charSpacing":0,"minWidth":20,"splitByGrapheme":false,"styles":{}},{"type":"rect","version":"4.2.0","originX":"left","originY":"top","left":200,"top":200,"width":200,"height":100,"fill":"#fcba03","stroke":null,"strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeDashOffset":0,"strokeLineJoin":"miter","strokeMiterLimit":4,"scaleX":1,"scaleY":1,"angle":0,"flipX":false,"flipY":false,"opacity":1,"shadow":null,"visible":true,"backgroundColor":"","fillRule":"nonzero","paintFirst":"fill","globalCompositeOperation":"source-over","skewX":0,"skewY":0,"rx":0,"ry":0},{"type":"rect","version":"4.2.0","originX":"left","originY":"top","left":80,"top":250,"width":200,"height":100,"fill":"#42f587","stroke":null,"strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeDashOffset":0,"strokeLineJoin":"miter","strokeMiterLimit":4,"scaleX":1,"scaleY":1,"angle":0,"flipX":false,"flipY":false,"opacity":1,"shadow":null,"visible":true,"backgroundColor":"","fillRule":"nonzero","paintFirst":"fill","globalCompositeOperation":"source-over","skewX":0,"skewY":0,"rx":0,"ry":0}],"background":"#ffffff"}
 
-    // const c = {
-    //     version: "4.2.0",
-    //     objects: [],
-    //     background: "#ffffff"
-    // }
+    let blankCanvas = {
+        version: "4.2.0",
+        objects: [],
+        background: "#ffffff"
+    }
 
     // canvas.clear()
     // canvas.loadFromJSON(c, canvas.renderAll.bind(canvas));
@@ -389,4 +393,61 @@ checkUser().then( async (user) => {
         canvas.clear();
         canvas.loadFromJSON(newCanvas, canvas.renderAll.bind(canvas));
     });
+
+
+
+    const rect = new fabric.Rect({
+        height: 100,
+        width: 200,
+        top: 200,
+        left: 200,
+        fill: '#fcba03',
+        id: 'rec'
+    })
+
+    canvas.add(rect)
+
+
+    const rect2 = new fabric.Rect({
+        height: 100,
+        width: 200,
+        top: 200,
+        left: 100,
+        fill: '#000000',
+    })
+
+    canvas.add(rect2)
+
+    console.log(canvas.toJSON(['rec']))
+
+    canvas.setActiveObject(rect2);
+    // const te = canvas.getActiveObjects()[0].toJSON();
+    canvas.getActiveObjects()[0].toJSON().id = 1
+
+    
+
+    // te.id = 1
+    // console.log(te)
+    // console.log(te.id)
+
+    const setId = (te) => {
+        if (!te.id) {
+            te.id = 'rec2'
+        } else {
+            te.id = 3
+        }
+        // te.id = 'rec2'
+        console.log(te)
+        console.log(te.id)
+    }
+
+    $('#test1').click(() => {
+        const te = canvas.getActiveObjects()[0].toJSON();
+        setId(te);
+    })
+    
+
+
+
+
 });
