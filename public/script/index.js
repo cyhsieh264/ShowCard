f5e92daa8d3a194dd0865b4c75e4d9fa3724daac
cyhsieh264 <cyhsieh264@gmail.com>
2020-12-26 16:24:07 +0800

    homepage carousel

--- public/script/index.js
+++ public/script/index.js
@@ -36,5 +36,20 @@ $('#new-card-btn').on('click', () => {
         .catch((error) => { console.error(error) });
 });
 
-$('.carousel').carousel();
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        autoPlay: 3000,
+        items : 4,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3],
+        center: true,
+        nav:true,
+        loop:true,
+        responsive: {
+            600: {
+            items: 4
+            }
+        }
+    });
+});
 
