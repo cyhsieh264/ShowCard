37129af3492297f347cf42a79cd7e345b48d43c0
cyhsieh264 <cyhsieh264@gmail.com>
2020-12-31 16:43:17 +0800

    move dispalyUser function to common.js

--- public/script/common.js
+++ public/script/common.js
@@ -29,3 +29,48 @@ const isValidEmailAddress = (email) => {
         return true
     }
 };
+
+// user info display
+
+const displayUser = async () => {
+    const userName = (await verifyUserToken(userToken)).name;
+    if (userToken && userName) {
+        $('#login').addClass('hide');
+        $('#start-with-showcard').addClass('hide');
+        $('#user').removeClass('hide');
+        $('#logout').removeClass('hide');
+        $('#user')[0].innerHTML = `${userName}`;
+    }
+};
+
+displayUser()
+
+$('#login').click(() => {
+    localStorage.setItem('history', '/studio.html');
+});
+
+$('#login-btn').click(() => {
+    localStorage.setItem('history', '/studio.html');
+});
+
+$('#logout').click(() => {
+    localStorage.removeItem('user_token');
+    swal({
+        title: 'Notification',
+        text: 'Sign Out Successfully',
+        type: 'warning',
+        icon: 'success',
+        confirmButtonText: 'OK'
+    }).then(() => {
+        location.href = '/';
+    });
+});
+
+$('#new-card-btn').on('click', () => {
+    axios.get('api/1.0/card/enroll')
+        .then((res) => {
+            const card = res.data.data.card;
+            location.href = `card.html?card=${card}`
+        })
+        .catch((error) => { console.error(error) });
+});
